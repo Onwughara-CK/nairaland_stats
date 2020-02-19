9bd690c05d0a0299079f46181a34d75a9bc2a0ba
Collins <kelechicollins.93@gmail.com>
2020-02-19 09:51:48 +0100

    Change the css selector from even selection to the correct class so as to only pick unhidden post and prevent the abnormal skipping of comments

--- nairaland_front_page_scraper/spiders/only_liked.py
+++ nairaland_front_page_scraper/spiders/only_liked.py
@@ -12,35 +12,40 @@ class OnlyLikedSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        for even in response.css('table[summary=\'posts\'] tr:nth-child(even)'):
-
+        for even in response.css('table[summary=\'posts\'] tr td[class=\'l w pd\']'):
             shares_str = even.css('p.s b[id ^=\'shb\']::text')
             likes_str = even.css('p.s b[id ^=\'lpt\']::text')
-            even_str = even.css('td[class="l w pd"] .narrow').get() or ''
+
+            even_str = even.css('td[class="l w pd"] .narrow').get()
             text = re.sub('<[^<>]+>', '--', even_str)
-            #text = even.css('.narrow::text').get()
             likes = 0
             shares = 0
             texts = None
 
             if shares_str:
                 shares = re.sub('\D+', '', shares_str.get())
-                #shares = re.sub('\D+', '', shares_str.get())
 
             if likes_str:
                 likes = re.sub('\D+', '', likes_str.get())
-                #likes = re.sub('\D+', '', likes_str.get())
 
             if text:
                 texts = text
 
-            yield {'likes': likes or 0,
-                   'shares': shares or 0,
-                   'comment': texts
-                   }
+            yield {
+                'likes': likes or 0,
+                'shares': shares or 0,
+                'comment': texts
+            }
 
         next_url = re.search('\d+', response.url).group()
         next_pages = response.css(
             f'div.nocopy > p:first-of-type > a[href ^=\'/{next_url}\']::attr(href)').getall()
         for next_page in next_pages:
             yield response.follow(url=next_page, callback=self.parse)
+
+# -------------- to-do--------------------------
+# ---work on text
+# blockquote to remain for quoted comments
+# Author should be included too
+
+# --scrape all post of a moniker
